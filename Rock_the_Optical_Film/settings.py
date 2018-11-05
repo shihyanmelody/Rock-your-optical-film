858965381888ef8f6fcf3ffba218e3172d7ce877
Shiyan (Melody) Zhang <40605494+shihyanmelody@users.noreply.github.com>
2018-11-04 21:17:16 -0600

    Update settings.py

--- Rock_the_Optical_Film/settings.py
+++ Rock_the_Optical_Film/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4+dtplsg0qnr&uafeus0fzh1^5sm%1e9jj10pssm)cer_siw7('
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
