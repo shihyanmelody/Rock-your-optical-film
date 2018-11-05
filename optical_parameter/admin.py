7eee36611bdc34b5e4e08ef77535abe85a331f0f
Shiyan (Melody) Zhang <40605494+shihyanmelody@users.noreply.github.com>
2018-11-04 21:11:05 -0600

    Delete admin.py

--- optical_parameter/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Pages, Refractiveindex, Extcoeff
-admin.site.register(Pages)
-admin.site.register(Refractiveindex)
-admin.site.register(Extcoeff)
